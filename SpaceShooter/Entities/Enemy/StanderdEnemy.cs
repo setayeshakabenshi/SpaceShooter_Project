5ab99aa1c2daf1c9e06b12acc229bd41e85df007
Zeinab Yaghoubi <z.yaghoubi69@gmail.com>
2026-06-30 18:29:59 +0330

    standerd enemy behaviour implement and drawing a sample of it

--- SpaceShooter/Entities/Enemy/StanderdEnemy.cs
+++ SpaceShooter/Entities/Enemy/StanderdEnemy.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SpaceShooter.Entities.Enemy;
+using System.Drawing;
 
 namespace SpaceShooter.Entities.Enemy
 {
-    class Class1
+    class StanderdEnemy:EnemyBase
     {
+        public StanderdEnemy(int x, int y, Rectangle bound) : base(x, y, 35, 35, 120f, 20, 100, bound)
+        {
+
+        }
+
+        public override void Update(float deltaTime)
+        {
+            Y += Speed * deltaTime;
+            if (Y > gameBounds.Height)
+                IsAlive = false;
+
+
+
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.FillRectangle(Brushes.Crimson, Bounds);
+        }
     }
 }
