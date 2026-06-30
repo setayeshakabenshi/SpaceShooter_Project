36aed28425cf1502133e52022a59cb655afc360f
Zeinab Yaghoubi <z.yaghoubi69@gmail.com>
2026-06-30 16:58:06 +0330

    player inherits as an object from game engin

--- SpaceShooter/Entities/Player.cs
+++ SpaceShooter/Entities/Player.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SpaceShooter.Core;
 
 namespace SpaceShooter.Entities
 {
-    class Player
+    class Player: GameEngin
+
     {
+        public int Speed { get; set; }
+
+        public Player(int x, int y, int width, int height, int speed)
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            Speed = speed;
+        }
+
+        public override void Update(float deltaTime)
+        {
+
+        }
+
     }
 }
