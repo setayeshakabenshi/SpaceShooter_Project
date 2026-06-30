4c621caf934ac06b087e0aa51c0d51103f19eb4d
Zeinab Yaghoubi <z.yaghoubi69@gmail.com>
2026-06-30 15:56:05 +0330

    adding behaviour of each bottons

--- SpaceShooter/Forms/MainMenuForm.Designer.cs
+++ SpaceShooter/Forms/MainMenuForm.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
-namespace SpaceShooter
+namespace SpaceShooter.Forms
 {
-    partial class Form1
+    partial class MainMenuForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,86 @@ namespace SpaceShooter
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Play = new System.Windows.Forms.Button();
+            this.btn_Shop = new System.Windows.Forms.Button();
+            this.btn_Option = new System.Windows.Forms.Button();
+            this.btn_About = new System.Windows.Forms.Button();
+            this.btn_Quit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Play
+            // 
+            this.btn_Play.Location = new System.Drawing.Point(332, 79);
+            this.btn_Play.Name = "btn_Play";
+            this.btn_Play.Size = new System.Drawing.Size(75, 23);
+            this.btn_Play.TabIndex = 0;
+            this.btn_Play.Text = "Play";
+            this.btn_Play.UseVisualStyleBackColor = true;
+            this.btn_Play.Click += new System.EventHandler(this.btn_Play_Click);
+            // 
+            // btn_Shop
+            // 
+            this.btn_Shop.Location = new System.Drawing.Point(332, 125);
+            this.btn_Shop.Name = "btn_Shop";
+            this.btn_Shop.Size = new System.Drawing.Size(75, 23);
+            this.btn_Shop.TabIndex = 1;
+            this.btn_Shop.Text = "Shop";
+            this.btn_Shop.UseVisualStyleBackColor = true;
+            this.btn_Shop.Click += new System.EventHandler(this.btn_Shop_Click);
+            // 
+            // btn_Option
+            // 
+            this.btn_Option.Location = new System.Drawing.Point(332, 175);
+            this.btn_Option.Name = "btn_Option";
+            this.btn_Option.Size = new System.Drawing.Size(75, 23);
+            this.btn_Option.TabIndex = 2;
+            this.btn_Option.Text = "Option";
+            this.btn_Option.UseVisualStyleBackColor = true;
+            this.btn_Option.Click += new System.EventHandler(this.btn_Option_Click);
+            // 
+            // btn_About
+            // 
+            this.btn_About.Location = new System.Drawing.Point(332, 238);
+            this.btn_About.Name = "btn_About";
+            this.btn_About.Size = new System.Drawing.Size(75, 23);
+            this.btn_About.TabIndex = 3;
+            this.btn_About.Text = "About";
+            this.btn_About.UseVisualStyleBackColor = true;
+            this.btn_About.Click += new System.EventHandler(this.btn_About_Click);
+            // 
+            // btn_Quit
+            // 
+            this.btn_Quit.Location = new System.Drawing.Point(332, 310);
+            this.btn_Quit.Name = "btn_Quit";
+            this.btn_Quit.Size = new System.Drawing.Size(75, 23);
+            this.btn_Quit.TabIndex = 4;
+            this.btn_Quit.Text = "Quit";
+            this.btn_Quit.UseVisualStyleBackColor = true;
+            this.btn_Quit.Click += new System.EventHandler(this.btn_Quit_Click);
+            // 
+            // MainMenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Quit);
+            this.Controls.Add(this.btn_About);
+            this.Controls.Add(this.btn_Option);
+            this.Controls.Add(this.btn_Shop);
+            this.Controls.Add(this.btn_Play);
+            this.Name = "MainMenuForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Play;
+        private System.Windows.Forms.Button btn_Shop;
+        private System.Windows.Forms.Button btn_Option;
+        private System.Windows.Forms.Button btn_About;
+        private System.Windows.Forms.Button btn_Quit;
     }
 }
 
--- SpaceShooter/Forms/MainMenuForm.cs
+++ SpaceShooter/Forms/MainMenuForm.cs
@@ -8,13 +8,53 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SpaceShooter
+namespace SpaceShooter.Forms
 {
-    public partial class Form1 : Form
+    public partial class MainMenuForm : Form
     {
-        public Form1()
+        public MainMenuForm()
         {
             InitializeComponent();
+        }
+
+        private void btn_Play_Click(object sender, EventArgs e)
+        {
+            
+
+            this.Hide();
+            GameForm gameform = new GameForm();
+            gameform.ShowDialog();
+
+            this.Show();
+        }
+
+        private void btn_Shop_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            ShopForm shopform = new ShopForm();
+            shopform.ShowDialog();
+            this.Show();
+        }
+
+        private void btn_Option_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            OptionForm optionform = new OptionForm();
+            optionform.ShowDialog();
+            this.Show();
+        }
+
+        private void btn_About_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            AboutForm aboutform = new AboutForm();
+            aboutform.ShowDialog();
+            this.Show();
+        }
+
+        private void btn_Quit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
