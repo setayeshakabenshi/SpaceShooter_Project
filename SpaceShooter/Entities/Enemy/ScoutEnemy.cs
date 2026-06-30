3622e0dcf5ab366caae1459aea8900725407d1a9
Zeinab Yaghoubi <z.yaghoubi69@gmail.com>
2026-06-30 18:33:54 +0330

    scout enemy movement implement

--- SpaceShooter/Entities/Enemy/ScoutEnemy.cs
+++ SpaceShooter/Entities/Enemy/ScoutEnemy.cs
@@ -11,5 +11,25 @@ namespace SpaceShooter.Entities.Enemy
     class ScoutEnemy:EnemyBase
     {
         private float AliveTime = 0f;
+
+        public ScoutEnemy(int x, int y, Rectangle bound) : base(x, y, 35, 34, 180f, 10, 150, bound)
+        {
+
+        }
+
+        public override void Update(float daltaTime)
+        {
+            AliveTime += daltaTime;
+            Y += Speed * daltaTime;
+            X += (float)Math.Sin(AliveTime * 4f) * 150f * daltaTime;
+
+            if (Y > Bounds.Height)
+                IsAlive = false;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.FillRectangle(Brushes.MediumSpringGreen, Bounds);
+        }
     }
 }
