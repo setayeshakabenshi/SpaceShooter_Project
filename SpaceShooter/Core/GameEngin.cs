4abc559ab154a35f464e445fae742a9d9b9739fa
Zeinab Yaghoubi <z.yaghoubi69@gmail.com>
2026-06-30 16:53:39 +0330

    adding abstract class for all objects that are supposed to move and have a locatoin

--- SpaceShooter/Core/GameEngin.cs
+++ SpaceShooter/Core/GameEngin.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace SpaceShooter.Core
 {
-    class GameEngin
+    public abstract class GameEngin
     {
+        public float X { get; set; }
+        public float Y { get; set; }
+
+        public int Width { get; protected set; }
+        public int Height { get; protected set; }
+
+        public bool IsAlive { get; set; } = true;
+
+        public Rectangle Bounds
+        {
+            get { return new Rectangle((int)X, (int)Y, Width, Height); }
+        }
+
+        public abstract void Update(float deltaTime);
+        public abstract void Draw(Graphics g);
     }
 }
