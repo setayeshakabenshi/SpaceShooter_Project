7c1d6ba6a835691d5cbdd5dc82349cbdc863fcb1
Zeinab Yaghoubi <z.yaghoubi69@gmail.com>
2026-06-30 18:32:16 +0330

    scout enemy implement - adding a time for calculatin movement

--- SpaceShooter/Entities/Enemy/ScoutEnemy.cs
+++ SpaceShooter/Entities/Enemy/ScoutEnemy.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SpaceShooter.Entities.Enemy;
+using System.Drawing;
 
 namespace SpaceShooter.Entities.Enemy
 {
-    class Class1
+    class ScoutEnemy:EnemyBase
     {
+        private float AliveTime = 0f;
     }
 }
