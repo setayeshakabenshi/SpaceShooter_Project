2789267f81f784fa993f03aa65607e8e3a57a1db
Zeinab Yaghoubi <z.yaghoubi69@gmail.com>
2026-06-30 17:57:03 +0330

    adding fildds for inherit from game engine as a object which can move and have location

--- SpaceShooter/Entities/Bullet.cs
+++ SpaceShooter/Entities/Bullet.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SpaceShooter.Core;
 
 namespace SpaceShooter.Entities
 {
-    class Bullet
+    class Bullet:GameEngin
     {
+        public int SpeedX { get; set; }
+        public int SpeedY { get; set; }
+        public bool IsPlayerBullet { get; set; }
+        
+
+        public Bullet(float x, float y, int width, int height, int speedX, int speedY, bool isPlayerBullet = true)
+
+        {
+            X = x;
+            Y = y;
+            Width = width;
+            Height = height;
+            SpeedX = speedX;
+            SpeedY = speedY;
+            IsPlayerBullet = isPlayerBullet;
+        }
     }
 }
