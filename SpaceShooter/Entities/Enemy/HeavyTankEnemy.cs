8d9cc105b0cf259626db97be2310e0a28653a7cf
setayesh jahedi <setayeshj76@gmail.com>
2026-07-03 01:33:44 +0330

    Adding the HeavyTank enemy type and finishing the logic for the way it behaves

---
+++ SpaceShooter/Entities/Enemy/HeavyTankEnemy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using SpaceShooter.Core;
+
+namespace SpaceShooter.Entities.Enemies
+{
+    public class HeavyTankEnemy : Enemy
+    {
+        private float _shootTimer = 0f;
+        private const float ShootInterval = 3f;
+        private const float Speed = 30f;
+
+        public HeavyTankEnemy(float x, float y) : base(x, y, 50, 50)
+        {
+            Health = 200;
+            MaxHealth = 200;
+            ScoreValue = 100;
+            CoinValue = 50;
+            Color = Color.DarkGray;
+            Velocity = new PointF(0, Speed);
+        }
+
+        public override void Update(float deltaTime)
+        {
+            base.Update(deltaTime);
+
+            _shootTimer += deltaTime;
+            if (_shootTimer >= ShootInterval)
+            {
+                Shoot();
+                _shootTimer = 0f;
+            }
+
+            if (Position.Y > 900)
+            {
+                IsActive = false;
+            }
+        }
+
+        protected override void Shoot()
+        {
+            float bulletSpeed = 250f;
+            for (int i = 0; i < 8; i++)
+            {
+                float angle = i * 45f * (float)Math.PI / 180f;
+                float vx = (float)Math.Cos(angle) * bulletSpeed;
+                float vy = (float)Math.Sin(angle) * bulletSpeed;
+
+                FireBullet(vx, vy);
+            }
+        }
+    }
+}
